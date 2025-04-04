89bff8b7f8cc7eb3b03c6f5e8c773a2ec643a05f
ulngollm <ulngollm@gmail.com>
2025-04-04 19:29:02 +0300

    use restrict instead of ban

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func init() {
 	}
 }
 
-const answerTimeout = 5 * time.Second
+const answerTimeout = 10 * time.Second
 
 const (
 	answerSpam = "yes"
@@ -57,12 +57,19 @@ func main() {
 
 func onJoin(c tele.Context) error {
 	// technically it is available to kick user from channel. Avoid this
-	if c.Chat().Type != tele.ChatSuperGroup || c.Chat().Type != tele.ChatGroup {
+	if c.Chat().Type != tele.ChatSuperGroup && c.Chat().Type != tele.ChatGroup {
 		return nil
 	}
 	//only if user join. Exclude left
-	if c.Update().ChatMember.NewChatMember.Role == tele.Kicked {
-		//todo снять бан
+	// также может быть restricted. Можно дать ему шанс ответить еще раз
+	newRole := c.Update().ChatMember.NewChatMember.Role
+	if newRole != tele.Member && newRole != tele.Restricted {
+		return nil
+	}
+	oldRole := c.Update().ChatMember.OldChatMember.Role
+	// этот же хендлер срабатывает, если с пользователя были сняты ограничения
+	// не присылать ничего в таком случае
+	if newRole != tele.Member && oldRole != tele.Restricted {
 		return nil
 	}
 
@@ -74,15 +81,21 @@ func onJoin(c tele.Context) error {
 		},
 	}
 
-	// Send the initial message with inline buttons
-	if err := c.Send(fmt.Sprintf("Привет, %s! Выбери, зачем пришел", c.Sender().FirstName), markup); err != nil {
-		return err
+	// use link to name tg://user?id=<user_id>
+	if err := c.Send(
+		fmt.Sprintf(
+			"Привет, [%s](tg://user?id=%d)\\! Выбери, зачем пришел",
+			c.Sender().FirstName,
+			c.Sender().ID,
+		), &tele.SendOptions{ParseMode: tele.ModeMarkdownV2}, markup); err != nil {
+		return fmt.Errorf("send: %w", err)
 	}
 
 	// Start a goroutine to handle the timeout
 	go func(c tele.Context) {
 		time.Sleep(answerTimeout)
-		//todo проверить, что пользователь еще состоит в чате
+		// проверить, что пользователь еще состоит в чате
+		// не банить пользователя, если он сам ушел
 		member := c.ChatMember().NewChatMember
 		m, err := c.Bot().ChatMemberOf(c.Chat(), member.User)
 		if err != nil {
@@ -95,24 +108,30 @@ func onJoin(c tele.Context) error {
 		// хак, как понять, что пользователь не ответил:
 		// если ответил - сообщение удалится. Если оно еще осталось - значит пользователь не ответил и будет забанен
 		// todo обработать кейсы, когда сообщение не удалилось по ошибке
-		// todo не банить пользователя, если он сам ушел
-		if err := c.Bot().Ban(c.Chat(), member); err != nil {
+		member.RestrictedUntil = time.Now().Add(1 * time.Hour).Unix() // в таком случае блочим только на час
+		if err := c.Bot().Restrict(c.Chat(), member); err != nil {
 			log.Printf("Failed to ban user after timeout: %v", err)
 		}
+		//	todo удалить сообщение. Для этого надо прокинуть id ?
 	}(c)
 
 	return nil
 }
 
 func onAnswer(c tele.Context) error {
+	//todo игнорить, если кнопку нажал другой пользователь
+	userToAsk := c.Callback().Message.Entities[0].User.ID
+	if c.Callback().Sender.ID != userToAsk {
+		return c.Respond(&tele.CallbackResponse{Text: "Это не вам."})
+	}
 	switch c.Data() {
 	case answerSpam:
 		r := &tele.CallbackResponse{Text: "you are banned"}
 		if err := c.Respond(r); err != nil {
 			return fmt.Errorf("respond: %w", err)
 		}
-		if err := c.Bot().Ban(c.Chat(), &tele.ChatMember{User: c.Callback().Sender}); err != nil {
-			return fmt.Errorf("bot.Ban: %w", err)
+		if err := c.Bot().Restrict(c.Chat(), &tele.ChatMember{User: c.Callback().Sender}); err != nil {
+			return fmt.Errorf("bot.Restrict: %w", err)
 		}
 	case answerOk:
 		r := &tele.CallbackResponse{Text: "you are ok"}
